b051a86d3506bcd3df2c3fe54a5e16329c68523b
Scott Crowder <scrowder@gmail.com>
2023-04-20 06:08:44 -0500

    start UI

---
+++ ui.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+THEME_COLOR = "#375362"
+QUESTION_FONT = ('Arial', 20, 'italic')
+
+
+class QuizUI:
+
+    def __init__(self):
+        self.window = Tk()
+
+
+        self.window.mainloop()
+
