8a520c22c6c4e7c675bc5b95aac04f5ce0dae3d0
Scott Crowder <scrowder@gmail.com>
2023-04-20 06:14:43 -0500

    add score label. add padding around canvas.

--- ui.py
+++ ui.py
@@ -7,15 +7,22 @@ QUESTION_FONT = ('Arial', 20, 'italic')
 class QuizUI:
 
     def __init__(self):
+        """
+        Create UI window
+        Create canvas with text that will wrap (width=280)
+        """
         self.window = Tk()
         self.window.title("Trivia Time!")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1,row=0)
 
         self.canvas = Canvas()
         self.canvas.config(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150, 125, text="sample", font=QUESTION_FONT, fill=THEME_COLOR,
                                                      width=280)
-        self.canvas.grid(column=0, row=1, columnspan=2)
+        self.canvas.grid(column=0, row=1, columnspan=2,pady=50)
 
 
         self.window.mainloop()
