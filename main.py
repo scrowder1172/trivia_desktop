55f96b805b53e7ed251223fe68fecddc1af93763
Scott Crowder <scrowder@gmail.com>
2023-04-20 06:56:47 -0500

    integrate data class and number of questions option

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from ui import QuizUI
-from data import question_data
+from data import QuestionData
+from question_model import Question
 
+
+number_of_questions = 20
+data = QuestionData(question_count=number_of_questions)
 
 question_bank = []
-for question in question_data:
+for question in data.get_question_data():
     question_text = question['question']
     question_answer = question['correct_answer']
-    new_question = "hold"
+    new_question = Question(q_text=question_text, q_answer=question_answer)
     question_bank.append(new_question)
 
+for question in question_bank:
+    print(question.text)
+
 quiz_ui = QuizUI()
+
