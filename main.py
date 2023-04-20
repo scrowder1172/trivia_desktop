b3e5d76b20927ecba0822ca981152665963bd9e4
Scott Crowder <scrowder@gmail.com>
2023-04-20 06:22:54 -0500

    integrate question data

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from ui import QuizUI
+from data import question_data
 
+
+question_bank = []
+for question in question_data:
+    question_text = question['question']
+    question_answer = question['correct_answer']
+    new_question = "hold"
+    question_bank.append(new_question)
 
 quiz_ui = QuizUI()
