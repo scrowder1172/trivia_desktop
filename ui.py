d36a1d93830582f3404f83778af9712702a3afbc
Scott Crowder <scrowder@gmail.com>
2023-04-20 06:12:10 -0500

    build window. create canvas. set canvas text to wrap.

--- ui.py
+++ ui.py
@@ -8,6 +8,14 @@ class QuizUI:
 
     def __init__(self):
         self.window = Tk()
+        self.window.title("Trivia Time!")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.canvas = Canvas()
+        self.canvas.config(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125, text="sample", font=QUESTION_FONT, fill=THEME_COLOR,
+                                                     width=280)
+        self.canvas.grid(column=0, row=1, columnspan=2)
 
 
         self.window.mainloop()
