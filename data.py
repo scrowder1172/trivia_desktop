782d0b9d3ea354a01bf1d22a0472dc1e49d7f8ce
Scott Crowder <scrowder@gmail.com>
2023-04-20 06:59:43 -0500

    set question_count data type

--- data.py
+++ data.py
@@ -3,11 +3,19 @@ import requests
 
 class QuestionData:
 
-    def __init__(self, question_count=10):
+    def __init__(self, question_count: int = 10):
+        """
+        Build class to retrieve questions
+        :param question_count: int
+        """
         self.question_count = question_count
         self.get_question_data()
 
     def get_question_data(self):
+        """
+        Query opentdb.com for list of questions
+        :return:
+        """
         api_parameters = {
             "amount": self.question_count,
             "type": "boolean"
