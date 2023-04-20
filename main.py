acaaea1ce32d75a5d8d62c94936854fd5c469b33
Scott Crowder <scrowder@gmail.com>
2023-04-20 06:57:08 -0500

    removed print statements

--- main.py
+++ main.py
@@ -13,8 +13,5 @@ for question in data.get_question_data():
     new_question = Question(q_text=question_text, q_answer=question_answer)
     question_bank.append(new_question)
 
-for question in question_bank:
-    print(question.text)
-
 quiz_ui = QuizUI()
 
